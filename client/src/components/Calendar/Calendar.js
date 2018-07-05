c70cd05f787bc19b4b45898298fe5bc5d26c6102
Ben <b.wayne@me.com>
2018-07-05 12:35:18 -0600

    new calendar

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,23 +1,117 @@
-// import React, { Component } from "react";
-// // import InfiniteCalendar from 'react-infinite-calendar';
-// import 'react-infinite-calendar/styles.css';
+import React, { Component } from "react";
+import dateFns from "date-fns";
 
-// let today = new Date();
-// let lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+class Calendar extends Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date()
+  }
 
-// class Calendar extends Component {
+  renderHeader() {
+    const dateFormat = "MMMM YYYY";
 
-//   render() {
-//     return(
-//     <InfiniteCalendar
-//       width={400}
-//       height={600}
-//       selected={today}
-//       disabledDays={[0,6]}
-//       minDate={lastWeek}
-//     />
-//     )
-//   }
-// }
+    return (
+      <div className="calHeader row flex-middle">
+        <div className="column column-start">
+          <div className="icon" onClick={this.prevMonth}>
+            Previous
+        </div>
+        </div>
+        <div className="column column-center">
+          <span>
+            {dateFns.format(this.state.currentMonth, dateFormat)}
+          </span>
+        </div>
+        <div className="column column-end" onClick={this.nextMonth}>
+          <div className="icon">Next</div>
+        </div>
+      </div>
+    );
+  }
 
-// export default Calendar;
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="column column-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+
+    const dateFormat = "D";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`column cell ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? "disabled"
+                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
+              }`}
+            key={day}
+            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  }
+
+  onDateClick = day => {
+    this.setState({
+      selectedDate: day
+    });
+  }
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+  }
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
+}
+
+export default Calendar;
