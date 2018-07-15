f02e06ec1eb22b3663e03513602cdfc08819c5b5
Strangebrewer <BKAShambala@gmail.com>
2018-07-15 07:27:38 -0600

    change some smart components to dumb components where possible

--- client/src/components/Elements/Footer/Footer.js
+++ client/src/components/Elements/Footer/Footer.js
@@ -2,20 +2,12 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./Footer.css"
 
-// Must pass in a relative or web url with an image in order for parallax to work
-
-class Footer extends React.Component {
-
-    render() {
-        return (
-            <footer className='footer'>
-                <a href="https://github.com/Kramerica5eva/Vandelay-Rentals" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <p>&#9400;2018 Kramerica Industries</p>
-                <Link className="btn-link" to={{pathname:"/", hash: "#about-us-para-header"}} role="button">About</Link>
-            </footer>
-        );
-    }
-}
-
+const Footer = () => (
+  <footer className='footer'>
+    <a href="https://github.com/Kramerica5eva/Vandelay-Rentals" target="_blank" rel="noopener noreferrer">GitHub</a>
+    <p>&#9400;2018 Kramerica Industries</p>
+    <Link className="btn-link" to={{ pathname: "/", hash: "#about-us-para-header" }} role="button">About</Link>
+  </footer>
+)
 
 export default Footer;
