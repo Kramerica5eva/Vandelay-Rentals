0dbfbdfa283433b5a2177ee10af3acc5efb3914f
Strangebrewer <BKAShambala@gmail.com>
2018-07-19 06:26:05 -0600

    add function to delete courses

--- controllers/adminCoursesController.js
+++ controllers/adminCoursesController.js
@@ -37,11 +37,37 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+
+  // All of this will be greatly simplified by storing the cart in a cookie/localStorage/state
+  //  If cookie or localStorage, we'll need to add functionality to check if a course is still available before checkout.
   remove: function (req, res) {
-    db.Course
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    db.TempRegistration.find({ courseId: req.params.id })
+      .then(dbModel => {
+        let promiseArray = [];
+        for (let i = 0; i < dbModel.length; i++) {
+          const element = dbModel[i];
+          const cartQuery = db.ShoppingCart.find(
+            { customerId: element.customerId },
+            { $pull: { TempRegistrations: element._id } }
+          )
+          promiseArray.push(cartQuery)
+          const tempRegQuery = db.TempRegistration.deleteOne(
+            { courseId: req.params.id }
+          )
+          promiseArray.push(tempRegQuery)
+        }
+        promiseArray.push(
+          db.Course
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+        )
+        Promise.all(promiseArray)
+          .then(values => res.json(values))
+          .catch(err => res.json(err));
+
+      }).catch(err => res.json(err));
+
   }
 };
