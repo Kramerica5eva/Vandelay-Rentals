3d4a27c41010cf7f14fe83679880d6f1b290cd6e
Corbin Moore <corbin.moore27@gmail.com>
2018-07-19 21:43:09 -0600

    about page

--- client/src/components/Elements/Footer/Footer.js
+++ client/src/components/Elements/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className='footer'>
     <a href="https://github.com/Kramerica5eva/Vandelay-Rentals" target="_blank" rel="noopener noreferrer">GitHub</a>
     <p>&#9400;2018 Kramerica Industries</p>
-    <Link className="btn-link" to={{ pathname: "/", hash: "#about-us-para-header" }} role="button">About</Link>
+    <Link className="btn-link" to="/about" role="button">About</Link>
   </footer>
 )
 
